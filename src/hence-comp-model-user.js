ac52f214dc1bb11e183d308102703d369c8829d4
Sean Homer <sean.homer@therefore.ca>
2015-07-23 13:41:25 -0400

    hence-comp - abstracting the way model components function to make an extendable core Hence component HenceModel - testing out the use of observables with behaviour and the HenceModel, running into unavoidable gotcha's, resulting in revising the way to build out the DRY model functionality

--- src/hence-comp-model-user.js
+++ src/hence-comp-model-user.js
@@ -3,7 +3,7 @@
  * @module hence-comp-model-user
  */
 import console from 'consoler';
-import HenceComp from 'hence-comp';
+import {HenceModel} from 'hence-comp';
 import _each from 'lodash/collection/each';
 
 let is = 'hence-comp-model-user';
@@ -12,39 +12,20 @@ let is = 'hence-comp-model-user';
  * HenceCompModelUser Component
  * @constructor
  */
-let HenceCompModelUser = HenceComp({
+let HenceCompModelUser = HenceModel({
   is, // auto set as is : is, es6 laziness joy!
   /********************************************************************************************************************
    * Initialization
    ********************************************************************************************************************/
-  properties: {
-    query: {
-      type: Object,
-      value: ()=> { return {}; }
-    },
-    state: {
-      type: Array,
-      value: null
-    }
-  },
+  properties: {},
 
   /*********************************************************************************************************************
    * Event Listeners
    ********************************************************************************************************************/
 
-  /**
-   * When working with listeners, if their target element doesn’t exist on the DOM you get a very basic nonspecific
-   * error 'Uncaught TypeError: Invalid value used as weak map key’!  Make sure to review the listeners you set up
-   * against you DOM elements. By default listeners look for IDs on elements so ‘myButton.tap’ will watch click/touches
-   * on a #myButton element in the component
-   */
-  listeners: {},
-
-
   /*********************************************************************************************************************
    * Element DOM Hooks
    ********************************************************************************************************************/
-
 
   /*********************************************************************************************************************
    * Element Behaviour
@@ -53,46 +34,34 @@ let HenceCompModelUser = HenceComp({
   /**
    * Manipulate the state loaded in from the schema to be suited for the expecting UI element
    */
-    transformState() {
-    let results = [];
-
-    if (!this.state) {
-      this.state = [];
-    }
-
-    this.state.forEach((entry)=> {
-      let user = {
-        title: `${entry.firstName} ${entry.lastName}'s Title`,
-        callToAction: {
-          label: 'Email user',
-          action: (e, model)=> {
-            alert('Email user' + entry.email);
-          }
+  _transformState(entry) {
+    let user = {
+      title: `${entry.firstName} ${entry.lastName}'s Title`,
+      callToAction: {
+        label: `Email user`,
+        action: (e, model)=> {
+          alert(`Email user: ${entry.email}`);
         }
-      };
-
-      if(entry.mySites) {
-        user.options = [];
-
-        _each(entry.mySites,(url, label)=> {
-          let site = {
-            label: label,
-            action: ()=> { window.location = url; }
-          };
-
-          user.options.push(site);
-        });
       }
+    };
 
-      results.push(user);
-    });
+    if (entry.mySites) {
+      user.options = [];
 
-    if (results.length === 1) {
-      results = results[0];
+      _each(entry.mySites, (url, label)=> {
+        let site = {
+          label: label,
+          action: ()=> { window.location = url; }
+        };
+
+        user.options.push(site);
+      });
     }
 
-    return results;
-  }
+    return user;
+  },
+
+  behaviors: []
 });
 
 export {is};
