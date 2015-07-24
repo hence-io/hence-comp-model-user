da0ff3af0bbd00f963f5523e4d90ae2b56626d6a
Sean Homer <sean.homer@therefore.ca>
2015-07-24 15:29:29 -0400

    - extrapolating the model updates back into the component generator to produce new model components in the new structure

--- src/dev.js
+++ src/dev.js
@@ -2,29 +2,6 @@
 
 import HenceCompModelUser from './hence-comp-model-user';
 
-let options = [
-  {
-    label: 'Test',
-    action: ()=> {
-      alert('this is my option!');
-    }
-  }
-];
-
-let callToAction = {
-  label: 'Sign Up Now!',
-  align: 'right', // left/center/right
-  action: (e, input)=> {
-    alert(`Successful submission with: ${input.value}`);
-  },
-  input: {
-    label: 'Enter your email here:',
-    type: 'text',
-    placeholder: 'Email'
-  }
-};
-
-
 HenceCompModelUser.appendElementTo({
   query : {
     id: 1
