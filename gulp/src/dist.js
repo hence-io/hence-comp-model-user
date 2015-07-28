6486ac37ad60a4a160c03393c6ad862ab581ff3d
Sean 'Blitz' Homer <sean.homer@therefore.ca>
2015-07-28 12:22:06 -0400

    - bringing the updated workflow back through the generator and current sample components

---
+++ gulp/src/dist.js
@@ -0,0 +1,4 @@
+'use strict';
+
+import HenceCompModelUser from './../../src/hence-comp-model-user';
+HenceCompModelUser.registerElement();
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,28 @@
-var gulp = require('gulp');
-var requireDir = require('require-dir');
+
+import gulp from 'gulp';
+import requireDir from 'require-dir';
 
 var srcDir = './src/';
 var tmpDir = './.tmp/';
+var buildSrcDir = './gulp/src/';
+
 
 global.comp = {
-  name: 'hence-comp-model-user'
+  name: 'hence-comp-model-user',
+  camel: 'HenceCompModelUser'
 };
-global.compassOptions = {
-  //config_file: './config.rb',
-  sass: srcDir,
-  css: tmpDir + 'css',
-  require: ['susy', 'modular-scale', 'breakpoint','font-awesome-sass']
-};
+
 // Specify paths & globbing patterns for tasks.
 global.paths = {
   // HTML sources.
   'html': srcDir + '**/*.html',
   // Bower
   'bower': './bower_components/',
+  // Build Src Dir
+  buildSrcDir: buildSrcDir,
   // Dist JS file
-  "distjs": srcDir + 'dist.js',
-  // Main JS file
+  'distjs': buildSrcDir + 'dist.js',
+  // Dev JS file
   'devjs': srcDir + 'index.js',
   // JS sources.
   'js': srcDir + '**/*.js',
@@ -45,6 +46,8 @@ global.paths = {
 
 // Require all tasks in the 'gulp' folder.
 requireDir('./gulp', {recurse: false});
+requireDir('./gulp/build', {recurse: false});
+requireDir('./gulp/development', {recurse: false});
 
 // Default task; start local server & watch for changes.
 gulp.task('default', ['serve']);
